fb55b3bd202e9b12bd8dce1c61770b841365a964
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-16 00:19:06 +0500

    Challenge

---
+++ script.js
@@ -0,0 +1,60 @@
+//<< FINDING EVEN POSITIONED ELEMENTS>>>
+
+
+// const getItemsAt = (array) => {
+//     let arr = [];
+//     for (let i = 0; i <= array.length; i++) {
+//         if (i % 2 === 0) {
+//             arr.push(array[i]);
+//         }
+//     }
+//     return arr;
+// }
+
+// console.log(getItemsAt([2, 4, 6, 8, 10])); => [2, 6, 10];
+
+
+//<<<< FINDING ODD POSITIONED NUMBERS>>>
+
+
+// const getItemsAt = (arr) => {
+//     let array = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i % 2 !== 0) {
+//             array.push(arr[i])
+//         }
+//     }
+//     return array
+// }
+
+// console.log(getItemsAt(["E", "D", "A", "B", "I", "T"]));  => ['D', 'B', 'T']
+
+
+
+//<<< FINDING THE LONGEST WORD IN STRING>>>
+
+
+// const findLongest = (str) => {
+//     var longestWord = str.split(' ')
+//     return longestWord.sort((a, b) => b.length - a.length)[0];
+// }
+
+// console.log(findLongest('The quick brown fox jumped over the lazy dog'));  => jumped
+
+
+//<<< ARRAY TO STRING>>>
+
+
+// var array = (['a', 'b', 'c', 'd', 'e', 'f']);
+// var string = array.join('');
+
+// console.log(string);
+
+
+//<<< ARRAY>>>>
+
+
+
+
+
+
