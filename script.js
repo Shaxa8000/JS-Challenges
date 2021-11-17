51c34b8865cb8c2e530bf9e25e718fa0a445f9e9
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-17 13:46:36 +0500

    another challenges

--- script.js
+++ script.js
@@ -141,6 +141,8 @@ const arrayClone = (arr) => {
 // var myColor = ['Red', 'Green', 'White', 'Black'];
 
 // var toString = myColor.join();
+// var toString = myColor.toString();
+// var toString = myColor.join('+');
 
 // console.log(toString);
 
@@ -190,7 +192,24 @@ const arrayClone = (arr) => {
 
 
 
-//<<< SWAP THE CASE OF EACH CHARACTER>>>
+//<<< NESTED ARRAYS >>>
+
+// var a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+// for (let i in a) {
+//     console.log('row ' + i);
+//     for (let j in a[i]) {
+//         console.log(' ' + a[i][j]);
+//     }
+// }
+
+
+//<<< CuRRENT DATE >>>
+
+var date = new Date();
+
+console.log(date);
+
 
 
 
