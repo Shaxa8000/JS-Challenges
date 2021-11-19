38b31186016ce2aafea15af375f4764113605aab
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-19 12:09:16 +0500

    new challenge

--- script.js
+++ script.js
@@ -230,6 +230,50 @@ const arrayClone = (arr) => {
 //<<<<<>>>>>>
 
 
+// unnamed
+let Rectangle = class {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+console.log(Rectangle.name);
+// output: "Rectangle"
+
+// named
+let Rectangle = class Rectangle2 {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+};
+console.log(Rectangle.name);
+// output: "Rectangle2"
+
+
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+  // Getter
+  get area() {
+    return this.calcArea();
+  }
+  // Method
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+const square = new Rectangle(10, 10);
+
+console.log(square.area); // 100
+
+
+
+
 
 
 
